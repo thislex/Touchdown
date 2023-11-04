3d0c9841674531770c7863c2b5b52ad5efe445c9
Lexter Tapawan <lexter.tapawan@gmail.com>
2023-11-04 12:59:22 +0000

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Lexter Tapawan on 04/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
